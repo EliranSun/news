17da06df27309a339e4039483fbb8d25cf77df69
Eliran Shemesh <112626543+eliransteps@users.noreply.github.com>
2024-09-22 12:30:15 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from "react";
+import { XMLParser } from "fast-xml-parser";
+
+const RssFeedComponent = () => {
+	const [feeds, setFeeds] = useState([]);
+	const rssFeedUrls = ["https://www.ynet.co.il/Integration/StoryRss2.xml"];
+
+	useEffect(() => {
+		const parser = new XMLParser();
+
+		const fetchAndParseFeeds = async () => {
+			try {
+				const allFeedsData = await Promise.all(
+					rssFeedUrls.map(async (url) => {
+						const response = await fetch(url, {
+							headers: {
+								"Content-Type": "application/xml",
+							},
+						});
+						const text = await response.text();
+						const result = parser.parse(text);
+						return result;
+					})
+				);
+
+				console.log({ allFeedsData });
+				setFeeds(allFeedsData);
+			} catch (error) {
+				console.error("Error fetching and parsing feeds:", error);
+			}
+		};
+
+		fetchAndParseFeeds();
+	}, []);
+
+	return (
+		<div>
+			{/* {feeds.map((feed, index) => (
+				<div key={index}>
+					<h2>{feed.rss.channel.title}</h2>
+					<ul>
+						{feed.rss.channel.item.map((item, idx) => (
+							<li key={idx}>
+								<a href={item.link}>{item.title}</a>
+							</li>
+						))}
+					</ul>
+				</div>
+			))} */}
+		</div>
+	);
+};
+
+export default RssFeedComponent;
