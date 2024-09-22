6edffadbc46c9358a6fd43522aad420b19f67b21
Eliran Shemesh <piro27@gmail.com>
2024-09-22 13:25:54 +0300

    wip rss with server api

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,38 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { XMLParser } from "fast-xml-parser";
 
 const RssFeedComponent = () => {
 	const [feeds, setFeeds] = useState([]);
-	const rssFeedUrls = ["https://www.ynet.co.il/Integration/StoryRss2.xml"];
 
 	useEffect(() => {
 		const parser = new XMLParser();
 
 		const fetchAndParseFeeds = async () => {
 			try {
-				const allFeedsData = await Promise.all(
-					rssFeedUrls.map(async (url) => {
-						const response = await fetch(url);
-						const text = await response.text();
-						const result = parser.parse(text);
-						return result;
-					})
-				);
+				const url = "https://walak.vercel.app/api/rss";
+				const response = await fetch(url);
+				const text = await response.text();
+				const result = parser.parse(text);
+				console.log({ result });
 
-				console.log({ allFeedsData });
-				setFeeds(allFeedsData);
+				const items = result?.rss?.channel?.item
+					.map((item) => {
+						const now = new Date();
+						const date = new Date(item.pubDate);
+						const diffTime = Math.abs(now - date);
+						const diffMinutes = Math.ceil(diffTime / (1000 * 60));
+
+						return {
+							title: item.title,
+							link: item.link,
+							language: result?.rss?.channel?.language,
+							diffMinutes,
+							date: item.pubDate,
+						};
+					})
+					.sort((a, b) => a.diffMinutes - b.diffMinutes);
+
+				setFeeds(items);
 			} catch (error) {
 				console.error("Error fetching and parsing feeds:", error);
 			}
@@ -29,20 +41,18 @@ const RssFeedComponent = () => {
 		fetchAndParseFeeds();
 	}, []);
 
+	console.log({ feeds });
+
 	return (
 		<div>
-			{/* {feeds.map((feed, index) => (
-				<div key={index}>
-					<h2>{feed.rss.channel.title}</h2>
-					<ul>
-						{feed.rss.channel.item.map((item, idx) => (
-							<li key={idx}>
-								<a href={item.link}>{item.title}</a>
-							</li>
-						))}
-					</ul>
+			{feeds.map((item, index) => (
+				<div
+					key={index}
+					dir={item.language === "en" ? "ltr" : "rtl"}>
+					<h1>{item.title}</h1>
+					<h2 dir="ltr">{item.diffMinutes} min ago</h2>
 				</div>
-			))} */}
+			))}
 		</div>
 	);
 };
