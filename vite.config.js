a06821b22f3cb628be2701d7e82fbcc2964c87d6
Eliran Shemesh <piro27@gmail.com>
2024-09-28 11:14:34 +0300

    Add vite support for manifest

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        // Add your manifest configuration here
+        name: 'News 2',
+        short_name: 'News 2',
+        description: 'Up to Date & Full of Anxiety',
+        theme_color: '#000000',
+        icons: [
+          // Add your icon configurations here
+        ]
+      }
+    })
+  ],
 })
