d07495b8d619196306a51986e3f3ac7e50e7a20c
Eliran Shemesh <piro27@gmail.com>
2024-09-29 16:17:43 +0300

    dark mode loader

--- src/Loader.jsx
+++ src/Loader.jsx
@@ -1,16 +1,22 @@
+const LoaderImage = ({ src, className }) => {
+	return (
+		<img
+			src={src}
+			alt="loading"
+			className={`w-1/2 object-cover block mx-auto filter grayscale ${className}`}
+		/>
+	);
+};
 export const Loader = () => {
 	return (
 		<div className="h-dvh w-full flex items-center justify-center">
-			<img
-				alt="loading"
+			<LoaderImage
+				className="dark:hidden"
 				src="https://cdn.dribbble.com/users/4072391/screenshots/19660250/media/d31593551e019ea191d9cd69cc542792.gif"
-				style={{
-					width: "50%",
-					objectFit: "cover",
-					display: "block",
-					margin: "0 auto",
-					filter: "grayscale(1)",
-				}}
+			/>
+			<LoaderImage
+				className="hidden dark:block"
+				src="/assets/loader-dark.gif"
 			/>
 		</div>
 	);
