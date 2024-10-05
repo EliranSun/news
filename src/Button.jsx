f4dd1ec797d0756dd42add8781787537a445b817
Eliran Shemesh <piro27@gmail.com>
2024-10-05 11:48:51 +0300

    Update Button.jsx

--- src/Button.jsx
+++ src/Button.jsx
@@ -2,7 +2,7 @@ export const Button = ({ children, className, ...rest }) => {
 	return (
 		<button
 			{...rest}
-			className={`h-10 bg-gray-100 dark:bg-gray-900 dark:text-white rounded text-xs p-1.5 flex items-center justify-center ${className}`}>
+			className={`w-full h-10 bg-gray-100 dark:bg-gray-900 dark:text-white rounded text-xs p-1.5 flex items-center justify-center ${className}`}>
 			{children}
 		</button>
 	);
