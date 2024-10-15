d292e5869cac46ab37c7a755f77426def0ef313f
Eliran Shemesh <piro27@gmail.com>
2024-10-15 17:38:29 +0300

    Condense view + year/week marks

--- src/features/TenThousandHours.jsx
+++ src/features/TenThousandHours.jsx
@@ -1,9 +1,11 @@
 import classNames from "classnames";
 import { useState } from "react";
 
-const tenThousandHoursArray = new Array(10000).fill(0);
+const BLOCK_COUNT = 1000;
+const tenThousandHoursArray = new Array(BLOCK_COUNT).fill(0);
 
 export const TenThousandHours = () => {
+    const [isCondensed, setIsCondensed] = useState(false);
     const [numberOfColumns, setNumberOfColumns] = useState(7);
     const [hours, setHours] = useState(() => {
         const searchParams = new URLSearchParams(window.location.search);
@@ -11,6 +13,10 @@ export const TenThousandHours = () => {
     });
 
     // const numberOfColumns = Math.ceil(window.innerWidth / (12 + 4 + 1));
+    const rowsCount = isCondensed
+        ? Math.ceil(BLOCK_COUNT / 38)
+        : Math.ceil(BLOCK_COUNT / numberOfColumns);
+
     return (
         <div className="flex flex-col items-center justify-center">
             <div className="flex items-center justify-center gap-4 mb-4">
@@ -28,19 +34,28 @@ export const TenThousandHours = () => {
                         searchParams.set('hours', newValue);
                         window.history.replaceState({}, '', `${window.location.pathname}?${searchParams}`);
                     }} />
-                <div>
-                    <h1>{numberOfColumns} days a week</h1>
-                    <h1>{numberOfColumns} hour a day</h1>
+                <div className="text-sm">
+                    <div>{numberOfColumns} days a week</div>
+                    <div>{numberOfColumns} hour a day</div>
                 </div>
+                <button onClick={() => setIsCondensed(!isCondensed)}>Condense</button>
             </div>
             <div
-                style={{ width: `${numberOfColumns * 12 + numberOfColumns * 4}px` }}
+                style={{
+                    width: isCondensed
+                        ? `${numberOfColumns * 52 + numberOfColumns * 4}px`
+                        : `${numberOfColumns * 12 + numberOfColumns * 4}px`
+                }}
                 className="relative flex items-center justify-start flex-wrap">
-                {new Array(100).fill(0).map((_, index) => (
+                {new Array(Math.round(isCondensed ? rowsCount : rowsCount / 50)).fill(0).map((_, index) => (
                     <h2
                         key={index}
-                        style={{ top: `${12 * 52 * index}px` }}
-                        className="absolute right-0 translate-x-full">
+                        style={{
+                            top: isCondensed
+                                ? `${10 * index}px`
+                                : `${12 * 52 * index}px`
+                        }}
+                        className="absolute right-0 translate-x-full text-[8px]">
                         YEAR {index}
                     </h2>
                 ))}
@@ -48,10 +63,12 @@ export const TenThousandHours = () => {
                     <div
                         key={index}
                         className={classNames({
-                            "text-[8px]": true,
-                            "size-3 m-0.5": true,
+                            "size-3 m-0.5": !isCondensed,
+                            "size-2 m-px": isCondensed,
                             "bg-slate-700": index < hours,
                             "bg-slate-400": index >= hours,
+                            "bg-red-500": index % 52 === 0,
+                            "bg-green-500": index % 7 === 0,
                         })}
                     />
                 ))}
