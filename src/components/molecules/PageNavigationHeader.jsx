eace6855d162f59972b02d9ef30fd4fae3865459
Eliran Shemesh <piro27@gmail.com>
2024-10-18 08:29:08 +0300

    Update PageNavigationHeader.jsx

--- src/components/molecules/PageNavigationHeader.jsx
+++ src/components/molecules/PageNavigationHeader.jsx
@@ -1,8 +1,8 @@
 import classNames from "classnames";
 
 export const PageNavigationHeader = ({
-    isSavedView,
-    setIsSavedView
+    view,
+    setView
 }) => {
     return (
         <div className="mb-4 flex justify-center gap-4 fixed top-0 py-5 bg-white dark:bg-black w-full z-10">
@@ -10,18 +10,27 @@ export const PageNavigationHeader = ({
                 className={classNames({
                     "text-sm font-bold w-20 text-center": true,
                     "opacity-50": isSavedView,
-                    "border-b border-slate-300": !isSavedView,
+                    "border-b border-slate-300": view === "feed"
                 })}
-                onClick={() => setIsSavedView(false)}>
+                onClick={() => setView("feed")}>
                 Feed
             </h1>
             <h1
                 className={classNames({
                     "text-sm font-bold w-20 text-center": true,
-                    "opacity-50": !isSavedView,
-                    "border-b border-slate-300": isSavedView,
+                    "opacity-50": isSavedView,
+                    "border-b border-slate-300": view === "feeds"
                 })}
-                onClick={() => setIsSavedView(true)}>
+                onClick={() => setView("feeds")}>
+                Feeds
+            </h1>
+            <h1
+                className={classNames({
+                    "text-sm font-bold w-20 text-center": true,
+                    "opacity-50": !isSavedView,
+                    "border-b border-slate-300": view === "saved",
+                })}
+                onClick={() => setView("saved")}>
                 Saved
             </h1>
         </div>
