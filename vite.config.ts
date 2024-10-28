a1cb378953f42ad27044f85e9ca1649480c7aed3
Eliran Shemesh <piro27@gmail.com>
2024-10-28 13:57:30 +0200

    wip cache clean

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,14 @@
 import {defineConfig} from "vite";
 import react from "@vitejs/plugin-react";
 import {VitePWA} from "vite-plugin-pwa";
-import {registerSW} from 'virtual:pwa-register';
-
-registerSW({immediate: true});
 
 // https://vitejs.dev/config/
 export default defineConfig({
    plugins: [
       react(),
       VitePWA({
-         registerType: "autoUpdate",
+         registerType: 'autoUpdate',
+         injectRegister: 'auto',
          devOptions: {
             enabled: true,
          },
