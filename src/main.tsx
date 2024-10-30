1f66b39022278d4d18f7c4951593256964a9d636
Eliran Shemesh <piro27@gmail.com>
2024-10-30 14:44:35 +0200

    Fix main should not include registerSW after removing PWA

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import {MainPage} from "./MainPage.jsx";
-import {registerSW} from 'virtual:pwa-register'
+import { MainPage } from "./MainPage.jsx";
+// import {registerSW} from 'virtual:pwa-register'
 
-registerSW({immediate: true});
+// registerSW({immediate: true});
 
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-   <React.StrictMode>
-      <MainPage/>
-   </React.StrictMode>
+	<React.StrictMode>
+		<MainPage />
+	</React.StrictMode>
 );
