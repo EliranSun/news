c8cb87e0d7f6b850c6390d6603e43cf179d75979
Eliran Shemesh <piro27@gmail.com>
2024-11-02 08:15:06 +0200

    Update sleep-tracker.tsx

--- src/components/organisms/sleep-tracker.tsx
+++ src/components/organisms/sleep-tracker.tsx
@@ -24,7 +24,6 @@ const View = ({ view, ...rest }) => {
 };
 
 const fetchDayData = (date) => {
-		const date = new Date();
 		const year = date.getFullYear();
 		const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are zero-indexed
 		const day = String(date.getDate()).padStart(2, '0');
