87aac72dc7515709ccf300294e532df9d051990a
Eliran Shemesh <piro27@gmail.com>
2025-01-28 10:26:53 +0200

    Clock page: use date input

--- src/components/organisms/clock.jsx
+++ src/components/organisms/clock.jsx
@@ -1,25 +1,44 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
+
+const getTimeLeft = (date) => {
+    const [year, month, day] = date.split('-').map(Number);
+    const targetDate = new Date(year, month - 1, day);
+    const now = new Date();
+    const diffTime = targetDate - now;
+
+    if (diffTime > 0) {
+        const diffDays = Math.floor(diffTime / (1000 * 60 * 60 * 24));
+        const diffHours = Math.floor((diffTime / (1000 * 60 * 60)) % 24);
+        const diffMinutes = Math.floor((diffTime / (1000 * 60)) % 60);
+
+        return { days: diffDays, hours: diffHours, minutes: diffMinutes };
+    }
+
+    return { days: 0, hours: 0, minutes: 0 };
+}
 
 export const Clock = () => {
     const [timeLeft, setTimeLeft] = useState({ days: 0, hours: 0, minutes: 0 });
+    const [date, setDate] = useState('');
+
+    const handleDateChange = useCallback((event) => {
+        const newDate = event.target.value;
+        const query = new URLSearchParams(window.location.search);
+        query.set('date', newDate);
+        window.history.replaceState(null, '', '?' + query.toString());
+
+        const timeLeft = getTimeLeft(newDate);
+        setTimeLeft(timeLeft);
+    }, []);
 
     useEffect(() => {
         const query = new URLSearchParams(window.location.search);
         const dateParam = query.get('date');
 
         if (dateParam) {
-            const [day, month, year] = dateParam.split('-').map(Number);
-            const targetDate = new Date(year + 2000, month - 1, day); // Adjust for 2-digit year
-            const now = new Date();
-
-            const diffTime = targetDate - now;
-            if (diffTime > 0) {
-                const diffDays = Math.floor(diffTime / (1000 * 60 * 60 * 24));
-                const diffHours = Math.floor((diffTime / (1000 * 60 * 60)) % 24);
-                const diffMinutes = Math.floor((diffTime / (1000 * 60)) % 60);
-
-                setTimeLeft({ days: diffDays, hours: diffHours, minutes: diffMinutes });
-            }
+            setDate(dateParam);
+            const timeLeft = getTimeLeft(dateParam);
+            setTimeLeft(timeLeft);
         }
 
         const interval = setInterval(() => {
@@ -45,10 +64,15 @@ export const Clock = () => {
     }, []);
 
     return (
-        <div className="text-center 
-        font-extrabold font-mono text-9xl
-        h-screen flex justify-center items-center">
-            {`${String(timeLeft.days).padStart(2, '0')}:${String(timeLeft.hours).padStart(2, '0')}:${String(timeLeft.minutes).padStart(2, '0')}`}
+        <div className="h-screen flex flex-col justify-center items-center">
+            <div className="font-extrabold font-mono text-9xl">
+                {`${String(timeLeft.days).padStart(2, '0')}:${String(timeLeft.hours).padStart(2, '0')}:${String(timeLeft.minutes).padStart(2, '0')}`}
+            </div>
+            <input
+                defaultValue={date}
+                type="date"
+                className="text-center text-xs fixed top-10"
+                onChange={handleDateChange} />
         </div>
     );
 };
