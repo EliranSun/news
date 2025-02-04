d542b73f11452d06cc4135d84c7aa3b113c60ace
Eliran Shemesh <piro27@gmail.com>
2025-02-04 09:24:31 +0200

    Fix incorrect variable name

--- src/components/molecules/FeedItem.jsx
+++ src/components/molecules/FeedItem.jsx
@@ -43,7 +43,7 @@ export const FeedItem = ({ item, onClick = () => { },
                             <div className={classNames({
                                 "h-32 overflow-y-auto": onlyTitle,
                             })}>
-                        {translatedTitle}
+                        {title}
                         </div>
                          {compact ?
                             <div>
