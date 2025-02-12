23cc0f85ae656bb9d9b6b3308b66fcf16ef49921
Eliran Shemesh <piro27@gmail.com>
2025-02-12 08:14:23 +0200

    Update FeedItem.jsx

--- src/components/molecules/FeedItem.jsx
+++ src/components/molecules/FeedItem.jsx
@@ -56,7 +56,7 @@ export const FeedItem = ({ item, onClick = () => { },
                                 <span>{item.diff.value}{item.diff.unit}</span>
                             </div> : null}
                     </h1>
-                    {(!queryResult ? null : (
+                    {!queryResult ? null : (
                         <p
                             dir={item.language === "he" ? "rtl" : "ltr"}
                             className="text-md mb-3">
