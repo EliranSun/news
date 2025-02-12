8e7a54847cf0acd1d7a3c3900b7f7df8860d9e7c
Eliran Shemesh <piro27@gmail.com>
2025-02-12 08:31:24 +0200

    Update Button.jsx

--- src/components/atoms/Button.jsx
+++ src/components/atoms/Button.jsx
@@ -1,11 +1,11 @@
 import PropTypes from "prop-types";
 
-export const Button = ({ children, className, ...rest }) => {
+export const Button = ({ children, full, className, ...rest }) => {
 	return (
 		<button
 			{...rest}
 			className={`border border-gray-100 text-xs p-1.5
-			 flex items-center justify-center ${className}`}>
+			 flex items-center justify-center ${full ? "w-full" : ""} ${className}`}>
 			{children}
 		</button>
 	);
