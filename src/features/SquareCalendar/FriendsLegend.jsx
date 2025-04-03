ab4a1fd3826bb9a3309d85c77e512e3a6eed11ec
Eliran Shemesh <piro27@gmail.com>
2025-04-03 17:32:08 +0300

    Friends legend UI changes

--- src/features/SquareCalendar/FriendsLegend.jsx
+++ src/features/SquareCalendar/FriendsLegend.jsx
@@ -1,11 +1,12 @@
 import classNames from "classnames";
 import { Calendars } from "./constants";
 import { getColorsClassList } from "./utils";
+
 export const FriendsLegend = ({ isActive }) => {
     if (!isActive) return null;
 
     return (
-        <legend>
+        <legend className="flex flex-wrap gap-2">
             {Calendars.Friends.legend.map((friend) => (
                 <div key={friend.name} className="flex items-center gap-2">
                     <div className={classNames({
--- src/features/SquareCalendar/SquareCalendar.jsx
+++ src/features/SquareCalendar/SquareCalendar.jsx
@@ -117,7 +117,6 @@ export default function SquareCalendar() {
                     )
                 })}
             </div>
-            <FriendsLegend isActive={calendar.key === Calendars.Friends.key} />
             <div className="flex justify-between items-center w-full border-2 shadow px-4 rounded-lg">
                 <div className="grid grid-cols-3 gap-2 max-w-[150px] my-2 border rounded-lg p-2">
                     <div className="flex justify-center items-center bg-gray-100 rounded-md p-2 opacity-0"></div>
@@ -143,6 +142,7 @@ export default function SquareCalendar() {
                     <ColorButton color="⬜️" onClick={() => updateColor('clear')} />
                 </div>
             </div>
+            <FriendsLegend isActive={calendar.key === Calendars.Friends.key} />
         </div>
     );
 }   
