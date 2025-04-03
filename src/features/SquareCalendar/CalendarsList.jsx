96445c67b31cc81d5692e15ce28628ad081bf817
Eliran Shemesh <piro27@gmail.com>
2025-04-03 23:51:32 +0300

    List to fit content

--- src/features/SquareCalendar/CalendarsList.jsx
+++ src/features/SquareCalendar/CalendarsList.jsx
@@ -15,7 +15,7 @@ export const CalendarsList = ({ onClick }) => {
     return (
         <div className="fixed top-0 left-0 w-full border space-y-2
         border-black dark:border-white p-8 rounded-lg z-10
-        h-full bg-white dark:bg-black backdrop-blur-sm">
+        h-fit bg-white dark:bg-black backdrop-blur">
             {Object.entries(groupedCalendars).map(([category, calendars]) => (
                 <div key={category}>
                     <h3 className="text-xl font-bold">{category}</h3>
