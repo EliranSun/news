7aa6bb49ffe9559293850bb944127fd65bde5dfe
Eliran Shemesh <piro27@gmail.com>
2025-04-03 08:22:36 +0300

    Fixed width for scroll

--- src/pages/SquareCalendar.jsx
+++ src/pages/SquareCalendar.jsx
@@ -105,7 +105,7 @@ export default function SquareCalendar() {
 
     return (
         <div className="p-4 h-dvh user-select-none space-y-12">
-            <h1 className="text-base font-bold flex flex-nowrap w-2/3 overflow-x-auto gap-4">
+            <h1 className="text-base font-bold flex flex-nowrap w-48 overflow-x-auto gap-4">
                 <CalendarButton>🟨 CSS</CalendarButton>
                 <CalendarButton>🟩 Read</CalendarButton>
                 <CalendarButton>🟦 Friends</CalendarButton>
