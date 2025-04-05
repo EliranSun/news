718aea00399952ee44008be5a27333d08d9d99ae
Eliran Shemesh <piro27@gmail.com>
2025-04-05 08:52:36 +0300

    Calendar button ui adjustments

--- src/features/SquareCalendar/CalendarButton.jsx
+++ src/features/SquareCalendar/CalendarButton.jsx
@@ -1,5 +1,10 @@
 import classNames from "classnames";
+
 export const CalendarButton = ({ children, isSelected, ...rest }) =>
-    <button className={classNames("min-w-20 shrink-0", {
-        "bg-black text-white": isSelected
-    })} {...rest}>{children}</button>;
+    <button 
+        className={classNames("min-w-20 shrink-0", {
+        "bg-black text-white": isSelected,
+        "text-sm flex": true,
+    })} {...rest}>
+    {children}
+    </button>;
