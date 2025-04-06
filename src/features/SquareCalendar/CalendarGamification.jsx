8be866d52402231ea9b8cf3dff068082151ecf25
Eliran Shemesh <piro27@gmail.com>
2025-04-06 07:55:11 +0300

    Fix import

--- src/features/SquareCalendar/CalendarGamification.jsx
+++ src/features/SquareCalendar/CalendarGamification.jsx
@@ -1,5 +1,5 @@
 import { getDaysSinceLastEntry, getStreakCount, getHighestStreakCount } from "./utils"
-import { Highscore, Streak } from "./CalendarList";
+import { Highscore, Streak } from "./CalendarsList";
 
 export const CalendarGamification = ({ calendar }) => {
      return null;
