4c607ad871634ab6081411cb80ae2f7c33685adb
Eliran Shemesh <piro27@gmail.com>
2025-04-07 23:49:28 +0300

    Adjust for new hours format

--- src/features/SquareCalendar/CalendarName.jsx
+++ src/features/SquareCalendar/CalendarName.jsx
@@ -2,11 +2,18 @@ import PropTypes from "prop-types";
 import classNames from "classnames";
 
 export const CalendarName = ({ calendar, daysSinceLastEntry }) => {
+    const yellow = calendar.yellowAfter * 24;
+    const red = calendar.redAfter * 24;
+    
     return (
         <div className={classNames("flex w-full", {
-            "text-red-500": calendar.showColorStatus && (daysSinceLastEntry === null || daysSinceLastEntry > calendar.redAfter),
-            "text-yellow-500": calendar.showColorStatus && daysSinceLastEntry > calendar.yellowAfter,
-            "text-green-500": calendar.showColorStatus && daysSinceLastEntry <= calendar.yellowAfter,
+            "text-red-500": 
+                calendar.showColorStatus && (daysSinceLastEntry === null || 
+                daysSinceLastEntry > red),
+            "text-yellow-500": calendar.showColorStatus && 
+                daysSinceLastEntry > yellow,
+            "text-green-500": calendar.showColorStatus && 
+                daysSinceLastEntry <= yellow,
         })}>
             <div className="w-6 shrink-0 overflow-hidden flex items-center justify-center text-sm">
                 {calendar.icon}
