c92ea105e503edf079658bf611d1437aa4612469
Eliran Shemesh <piro27@gmail.com>
2025-04-07 09:04:17 +0300

    Columns

--- src/features/SquareCalendar/ExportImport.jsx
+++ src/features/SquareCalendar/ExportImport.jsx
@@ -2,7 +2,7 @@ import { exportCalendarData, importCalendarData } from "./utils";
 
 export const ExportImport = () => {
     return (
-        <div className="flex gap-2">
+        <div className="flex flex-col gap-2">
             <button onClick={exportCalendarData}>
                 EXPORT
             </button>
