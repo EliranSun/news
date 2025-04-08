2c99e3a5468ada662695f82e4ca275e620bd1a24
Eliran Shemesh <piro27@gmail.com>
2025-04-08 17:47:41 +0300

    Fix color condition

--- src/features/SquareCalendar/CalendarsStrip.jsx
+++ src/features/SquareCalendar/CalendarsStrip.jsx
@@ -5,7 +5,7 @@ import { getDaysSinceLastEntry } from "./utils";
 import { useMemo } from "react";
 
 export const CalendarsStrip = ({ selectedCalendar, onCalendarClick }) => {
-    const daysSinceLastEntry = useMemo(() => getDaysSinceLastEntry(selectedCalendar.key), [selectedCalendar.key]);
+    const hoursSinceLast = useMemo(() => getDaysSinceLastEntry(selectedCalendar.key), [selectedCalendar.key]);
 
     return (
         <div className="text-base font-bold flex flex-nowrap w-[calc(99vw-99px)] overflow-x-auto gap-4">
@@ -15,8 +15,8 @@ export const CalendarsStrip = ({ selectedCalendar, onCalendarClick }) => {
                         onClick={() => onCalendarClick(item)}
                         isSelected={selectedCalendar.key === item.key}
                         color={
-                            (daysSinceLastEntry >= selectedCalendar.redAfter || !daysSinceLastEntry) ? "red" :
-                                daysSinceLastEntry >= selectedCalendar.yellowAfter ? "yellow" :
+                            ((hoursSinceLast * 24) >= selectedCalendar.redAfter || !hoursSinceLast) ? "red" :
+                                (hoursSinceLast * 24) >= selectedCalendar.yellowAfter ? "yellow" :
                                     "green"
                         }>
                         {item.icon} {item.name.slice(0, 3)}
