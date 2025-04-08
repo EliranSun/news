4a1b1bbf7d478e95b17c82c854f2e60789853ea7
Eliran Shemesh <piro27@gmail.com>
2025-04-08 20:02:25 +0300

    No slice - info is important

--- src/features/SquareCalendar/CalendarLegend.jsx
+++ src/features/SquareCalendar/CalendarLegend.jsx
@@ -6,13 +6,13 @@ export const CalendarLegend = ({ isActive, legend = [] }) => {
     if (!isActive || !legend) return null;
 
     return (
-        <legend className="flex flex-wrap gap-2">
+        <legend className="w-full flex flex-wrap gap-2">
             {legend.map((item) => (
                 <div key={item.name} className="flex items-center text-xs gap-2">
                     <div className={classNames(getColorsClassList(item.color), {
                         "size-2 rounded-md": true,
                     })} />
-                    <span>{item.name.slice(0,5)}</span>
+                    <span>{item.name}</span>
                 </div>
             ))}
         </legend>
