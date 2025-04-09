60bf98fe2a47ebc5112857c0d304d4cb163e1e3e
Eliran Shemesh <piro27@gmail.com>
2025-04-09 20:23:15 +0300

    Width screen?

--- src/features/SquareCalendar/SquareCalendar.jsx
+++ src/features/SquareCalendar/SquareCalendar.jsx
@@ -91,7 +91,7 @@ export default function SquareCalendar() {
                     </button>
 
                 </div> */}
-                <div className="flex w-full justify-between px-4">
+                <div className="flex w-screen justify-between px-4">
                     <CalendarName
                         calendar={calendar}
                         daysSinceLastEntry={daysSinceLastEntry} />
