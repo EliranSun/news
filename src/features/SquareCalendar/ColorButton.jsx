703447cc056dcd503547deafb2c12297592a967d
Eliran Shemesh <piro27@gmail.com>
2025-04-09 08:03:08 +0300

    Show legend

--- src/features/SquareCalendar/ColorButton.jsx
+++ src/features/SquareCalendar/ColorButton.jsx
@@ -1,12 +1,13 @@
 import classNames from "classnames";
 import { getColorsClassList } from "./utils";
 
-export const ColorButton = ({ color, onClick }) => {
+export const ColorButton = ({ color, onClick, legend }) => {
     return (
         <button
             onClick={onClick}
-            className="flex justify-center items-center rounded-full p-2">
+            className="flex flex-col justify-center items-center rounded-full p-2">
             <div className={classNames("size-4", getColorsClassList(color))} />
+            {legend && <label className="text-xs">{legend.name}</legend>}
         </button>
     );
 };
