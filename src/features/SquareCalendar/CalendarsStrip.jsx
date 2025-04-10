d45c63f96cf58c76faddfc1998ccb5efff4e667b
Eliran Shemesh <piro27@gmail.com>
2025-04-10 08:03:40 +0300

    Fix key logic?

--- src/features/SquareCalendar/CalendarsStrip.jsx
+++ src/features/SquareCalendar/CalendarsStrip.jsx
@@ -5,13 +5,12 @@ import PropTypes from "prop-types";
 export const CalendarsStrip = ({ selectedCalendar, onCalendarClick }) => {
     return (
         <div className="text-base font-bold flex flex-nowrap w-screen px-4 overflow-x-auto gap-4">
-            {Object.entries(Calendars).map(([key, item]) => {
-                console.log({ key, item });
+            {Object.values(Calendars).map(item => {
                 return (
-                    <div key={key} className="h-fit" id={key}>
+                    <div key={item.key} className="h-fit" id={item.key}>
                         <CalendarButton
                             onClick={() => onCalendarClick(item)}
-                            calendar={{ ...item, key }}
+                            calendar={item}
                             isSelected={selectedCalendar.key === item.key}>
                             {item.icon}
                         </CalendarButton>
