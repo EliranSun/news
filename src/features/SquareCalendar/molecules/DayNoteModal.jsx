1091a87501b644f2bf214b53ec290ef77e5dd986
Eliran Shemesh <piro27@gmail.com>
2025-04-15 00:02:51 +0300

    Deliberate update to prevent mistake deletion

--- src/features/SquareCalendar/molecules/DayNoteModal.jsx
+++ src/features/SquareCalendar/molecules/DayNoteModal.jsx
@@ -1,5 +1,5 @@
 import classNames from "classnames";
-import { X } from "@phosphor-icons/react";
+import { X, Check } from "@phosphor-icons/react";
 import PropTypes from "prop-types";
 import { useState, useEffect } from "react";
 import { createPortal } from "react-dom";
@@ -31,12 +31,13 @@ useEffect(() => setNote(initialNote), [initialNote]);
                 value={note}
                 placeholder="Note"
                 onChange={event => setNote(event.target.value)}
-                onBlur={() => onUpdate(note)}
+                
                 className={classNames({
                     "m-4 p-4 rounded-lg font-mono": true,
                     "border w-[calc(100%-2rem)] h-[calc(100%-15rem)] min-h-10": true,
                 })}
             />
+            <div className="flex gap-4 items-center justify-center">
             <X
                 size={20}
                 color="black"
@@ -44,6 +45,14 @@ useEffect(() => setNote(initialNote), [initialNote]);
                 className="absolute bottom-20 inset-x-0 border
                 m-auto bg-white dark:bg-gray-900 rounded-full size-10 p-2"
                 onClick={onClose} />
+                <Check
+                size={20}
+                color="black"
+                weight="bold"
+                className="absolute bottom-20 inset-x-0 border
+                m-auto bg-white dark:bg-gray-900 rounded-full size-10 p-2"
+                onClick={() => onUpdate(note)} />
+                </div>
         </motion.div>,
         document.getElementById('note-modal-portal') || document.body
 
