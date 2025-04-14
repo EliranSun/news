646527bec2e8364e74ee21a43ef99514efd7f002
Eliran Shemesh <piro27@gmail.com>
2025-04-14 11:40:05 +0300

    Labels

--- src/features/SquareCalendar/molecules/Navbar.jsx
+++ src/features/SquareCalendar/molecules/Navbar.jsx
@@ -2,12 +2,13 @@ import PropTypes from "prop-types";
 import { List, TrayArrowDown, TrayArrowUp, Calendar, CalendarDot, CalendarDots } from "@phosphor-icons/react";
 import { exportCalendarData, importCalendarData } from "../utils";
 
-const NavbarButton = ({ icon: Icon, onClick }) => {
+const NavbarButton = ({ icon: Icon, onClick, label }) => {
     return (
         <button onClick={onClick} className="flex rounded-none
          items-center justify-center h-16 w-full
          bg-transparent border-none">
             <Icon size={24} />
+            <label className="text-xs">{label}</label>
         </button>
     );
 };
@@ -21,12 +22,12 @@ export const Navbar = ({ onListClick }) => {
     return (
         <div className="fixed h-24 bg-white dark:bg-black
          bottom-0 border-t w-screen grid grid-cols-6 items-start justify-center">
-            <NavbarButton icon={List} onClick={onListClick} />
-            <NavbarButton icon={Calendar} onClick={() => { }} />
-            <NavbarButton icon={CalendarDot} onClick={() => { }} />
-            <NavbarButton icon={CalendarDots} onClick={() => { }} />
-            <NavbarButton icon={TrayArrowDown} onClick={importCalendarData} />
-            <NavbarButton icon={TrayArrowUp} onClick={exportCalendarData} />
+            <NavbarButton label="list" icon={List} onClick={onListClick} />
+            <NavbarButton label="day" icon={Calendar} onClick={() => { }} />
+            <NavbarButton label="month" icon={CalendarDot} onClick={() => { }} />
+            <NavbarButton label="year" icon={CalendarDots} onClick={() => { }} />
+            <NavbarButton label="import" icon={TrayArrowDown} onClick={importCalendarData} />
+            <NavbarButton label="export" icon={TrayArrowUp} onClick={exportCalendarData} />
         </div>
     );
 };
