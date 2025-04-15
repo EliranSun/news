63a9667971aa911113110d583a86de495b0b4c52
Eliran Shemesh <piro27@gmail.com>
2025-04-15 14:02:28 +0300

    Slice content

--- src/features/SquareCalendar/organism/CalendarDayView.jsx
+++ src/features/SquareCalendar/organism/CalendarDayView.jsx
@@ -2,8 +2,10 @@ import classNames from "classnames";
 import { getColorsClassList } from "../utils";
 import PropTypes from "prop-types";
 import { format } from "date-fns";
+import { useState } from "react";
+
 export const CalendarDayView = ({ data }) => {
-    console.log({ data });
+    const [isNoteExpanded, setIsNoteExpanded] = useState(false);
 
     return (
         <div className="flex flex-col gap-1 h-[80vh] w-screen overflow-y-auto pb-20">
@@ -12,15 +14,20 @@ export const CalendarDayView = ({ data }) => {
                 const dayNumber = format(item.date, "dd");
                 const month = format(item.date, "MMM");
                 return (
-                    <div className={classNames({
-                        "flex gap-4 items-start bg-gray-50 odd:bg-gray-100": true,
-                        "rounded-lg px-4 py-1 text-sm": true,
-                        "mb-8": day === "Sat",
-                    })} key={item.date}>
+                    <div
+                        key={item.date}
+                        onClick={() => setIsNoteExpanded(!isNoteExpanded)}
+                        className={classNames({
+                            "flex gap-4 items-start bg-gray-50 odd:bg-gray-100": true,
+                            "rounded-lg px-4 py-1 text-sm": true,
+                            "mb-8": day === "Sat",
+                        })} >
                         <div className={classNames(getColorsClassList(item.color), {
                             "size-4 rounded shrink-0": true,
                         })} />
-                        <div>{dayNumber} {month} {day}{item.note ? " - " : ""}{item.note}</div>
+                        <div>{day.slice(0, 1)}{dayNumber}{month.slice(0, 1)}{item.note ? " - " : ""}{
+                            isNoteExpanded ? item.note : item.note?.slice(0, 30)
+                        }</div>
                     </div>
                 );
             })}
