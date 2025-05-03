e9bea52b5348197b709c0c0a37efe5c16f8210e2
Eliran Shemesh <piro27@gmail.com>
2025-05-03 09:54:21 +0300

    Two strips

--- src/features/SquareCalendar/molecules/CalendarsStrip.jsx
+++ src/features/SquareCalendar/molecules/CalendarsStrip.jsx
@@ -5,7 +5,20 @@ import PropTypes from "prop-types";
 export const CalendarsStrip = ({ selectedCalendar, onCalendarClick }) => {
     return (
         <div className="flex flex-col gap-1 w-full">
-            {/* <h1 className="text-base font-bold my-0 inter-500">Calendars</h1> */}
+            <div className="text-base font-bold flex flex-nowrap overflow-x-auto gap-2">
+                {Object.values(Calendars).map(item => {
+                    return (
+                        <div key={item.key} className="h-fit" id={item.key}>
+                            <CalendarButton
+                                onClick={() => onCalendarClick(item)}
+                                calendar={item}
+                                isSelected={selectedCalendar.key === item.key}>
+                                {item.icon}
+                            </CalendarButton>
+                        </div>
+                    )
+                })}
+            </div>
             <div className="text-base font-bold flex flex-nowrap overflow-x-auto gap-2">
                 {Object.values(Calendars).map(item => {
                     return (
