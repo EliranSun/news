159f2dac1ded163cc4b6df17526a9030c2fa5a4f
Eliran Shemesh <piro27@gmail.com>
2025-05-06 00:49:03 +0300

    Fix incorrect var

--- src/features/SquareCalendar/organism/FeedView.jsx
+++ src/features/SquareCalendar/organism/FeedView.jsx
@@ -37,7 +37,7 @@ const FeedItem = ({
                 setSelectedDate={newDate => {
                     setSelectedDate(newDate);
                     const dayItem = data.find(item => isSameDay(item.date, newDate));
-                    setSelectedDateNote(dayItem?.note || "");
+                    setNote(dayItem?.note || "");
                 }}
                 onColorSelect={(color) => {
                     let newData;
