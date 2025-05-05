ac5d1d10204f9b9645a5f25bf9dec0679d0a8f68
Eliran Shemesh <piro27@gmail.com>
2025-05-05 08:01:04 +0300

    Update StreakBadge.jsx

--- src/features/SquareCalendar/atoms/StreakBadge.jsx
+++ src/features/SquareCalendar/atoms/StreakBadge.jsx
@@ -2,13 +2,13 @@ import PropTypes from "prop-types";
 import { Badge } from "./Badge";
 import { Fire } from "@phosphor-icons/react";
 
-export const StreakBadge = ({ streak, showValue = true, size }) => {
-    if (streak === 0) return null;
+export const StreakBadge = ({ streak, visible = true, size }) => {
+    if (!visible || streak === 0) return null;
 
     return (
         <Badge className="h-fit" size={size}>
             <Fire size={size === "big" ? 24 : 16} color="red" />
-            {showValue && <span>{streak}d</span>}
+            <span>{streak}d</span>
         </Badge>
     )
 };
