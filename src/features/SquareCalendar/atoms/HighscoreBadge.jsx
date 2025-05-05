7d8869a57c66148425c42159948193fa79600eaf
Eliran Shemesh <piro27@gmail.com>
2025-05-05 08:01:35 +0300

    Update HighscoreBadge.jsx

--- src/features/SquareCalendar/atoms/HighscoreBadge.jsx
+++ src/features/SquareCalendar/atoms/HighscoreBadge.jsx
@@ -2,13 +2,13 @@ import PropTypes from "prop-types";
 import { Badge } from "./Badge";
 import { Trophy } from "@phosphor-icons/react";
 
-export const HighscoreBadge = ({ highscore, streak, showValue = true, size }) => {
-    if (highscore === 0 || streak === highscore) return null;
+export const HighscoreBadge = ({ highscore, streak, size }) => {
+    if (!visible || highscore === 0 || streak === highscore) return null;
 
     return (
         <Badge className="h-fit" size={size}>
             <Trophy size={size === "big" ? 24 : 16} color="gold" />
-            {showValue && <span>{highscore}d</span>}
+            <span>{highscore}d</span>
         </Badge>
     )
 };
