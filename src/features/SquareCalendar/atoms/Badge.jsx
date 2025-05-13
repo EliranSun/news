28061f2b8910ecc521855d4a99295137aff88a35
Eliran Shemesh <piro27@gmail.com>
2025-05-13 20:24:47 +0300

    Update Badge.jsx

--- src/features/SquareCalendar/atoms/Badge.jsx
+++ src/features/SquareCalendar/atoms/Badge.jsx
@@ -2,13 +2,19 @@ import PropTypes from "prop-types";
 import classNames from "classnames";
 
 // TODO: Refactor to Pill
-export const Badge = ({ children, className = "", textBlack = false, size = "small" }) => {
+export const Badge = ({ 
+children, 
+className = "", 
+textBlack = false,
+transparent = false,
+size = "small" }) => {
     return (
         <div className={classNames({
             "w-fit flex gap-1": true,
             "items-center justify-center overflow-hidden": true,
             "px-2": true,
-            "text-sm bg-black  rounded-xl": true,
+            "bg-black": !transparent,
+            "text-sm  rounded-xl": true,
             "text-black dark:text-white": textBlack,
             "text-white": !textBlack,
             "py-1 px-2": size === "big",
