d0874a1077cb47ac0a1ed1c6add464974a612265
Eliran Shemesh <piro27@gmail.com>
2025-05-16 14:22:02 +0300

    Fix syntax

--- src/features/SquareCalendar/atoms/DaySquare.jsx
+++ src/features/SquareCalendar/atoms/DaySquare.jsx
@@ -28,7 +28,7 @@ onClick, onDoubleClick, data, size = "small" }) => {
                 "opacity-0": dayObj.previousMonth,
                 // "border-2 border-black dark:border-white": !dayObj.previousMonth && isToday,
             })}>
-            {dayObj?.note ? "·" || ""}
+            {dayObj?.note ? "·" : ""}
             {(!dayObj.previousMonth && isToday)
                 ? dayObj.date.toLocaleString('default', { day: 'numeric' })
                 : null}
