7a50d7049ac43e6b2133ea361aec06ba683141fb
Eliran Shemesh <piro27@gmail.com>
2025-06-10 18:25:52 +0300

    test alignment

--- src/features/SquareCalendar/molecules/CalendarGamification.jsx
+++ src/features/SquareCalendar/molecules/CalendarGamification.jsx
@@ -15,10 +15,10 @@ export const CalendarGamification = ({ calendar, size, variant = "default" }) =>
 
     return (
         <div className={classNames({
-            "flex rounded-full": true,
+            "flex justify-center items-center gap-2 rounded-full": true,
             "bg-stone-800": variant === "default",
             "text-stone-800": variant === "plain",
-            "p-2 text-center gap-2 justify-center items-center": true, 
+            "p-2 text-center": true, 
         })}>
             <DaysSinceBadge
                 calendar={calendar}
